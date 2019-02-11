3b53461fee4030c74ab0241d0ada3d0689e0cc36
tomek <wasilewski.tg@gmail.com>
2019-02-12 00:59:19 +0100

    Main Controller TopMenuButtons TopMenuButtonsController

--- src/main/java/myprojects/mylibrary/controllers/MainController.java
+++ src/main/java/myprojects/mylibrary/controllers/MainController.java
@@ -1,4 +1,22 @@
 package myprojects.mylibrary.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
+
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize(){
+
+        topMenuButtonsController.setMainController(this);
+
+    }
+//odc 27 - n
+
+
 }
---
+++ src/main/java/myprojects/mylibrary/controllers/TopMenuButtonsController.java
@@ -0,0 +1,24 @@
+package myprojects.mylibrary.controllers;
+
+import javafx.fxml.FXML;
+
+public class TopMenuButtonsController {
+
+    private MainController mainController;
+
+    @FXML
+    public void openLibrary() {
+    }
+
+    @FXML
+    public void openBooks() {
+    }
+
+    @FXML
+    public void openStats() {
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+}
